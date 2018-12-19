89a59aa0e4235a51508766252e41ff998578a662
Deven Phillips <deven.phillips@redhat.com>
2018-12-19 12:51:44 -0500

    Add Quasar to VueJS app

--- src/main/frontend/src/main.js
+++ src/main/frontend/src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue';
 import router from './router';
 import './registerServiceWorker';
 
+import './styles/quasar.styl'
+import 'quasar-framework/dist/quasar.ie.polyfills'
+import 'quasar-extras/animate'
+import 'quasar-extras/material-icons'
+import Quasar from 'quasar'
+
+Vue.use(Quasar, {
+  config: {}
+ })
+
 Vue.config.productionTip = false;
 
 new Vue({
