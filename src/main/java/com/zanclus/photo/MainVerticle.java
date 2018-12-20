f129ef5c5beecf199e8f26e7f0b4f8bff3825d7e
Deven Phillips <deven.phillips@redhat.com>
2018-12-19 19:15:16 -0500

    Started work on RXStream startup and config loading

--- src/main/java/com/zanclus/photo/MainVerticle.java
+++ src/main/java/com/zanclus/photo/MainVerticle.java
@@ -1,62 +1,172 @@
 package com.zanclus.photo;
 
-import io.vertx.core.AbstractVerticle;
-import io.vertx.core.http.HttpServer;
+import io.reactivex.Maybe;
+import io.vertx.config.ConfigRetrieverOptions;
+import io.vertx.config.ConfigStoreOptions;
+import io.vertx.core.http.HttpMethod;
 import io.vertx.core.http.HttpServerOptions;
-import io.vertx.ext.web.api.contract.openapi3.OpenAPI3RouterFactory;
-import io.vertx.ext.web.Router;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.bridge.PermittedOptions;
+import io.vertx.ext.web.handler.sockjs.BridgeOptions;
+import io.vertx.reactivex.config.ConfigRetriever;
+import io.vertx.reactivex.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.reactivex.core.http.HttpServer;
+import io.vertx.reactivex.ext.web.Router;
+import io.vertx.reactivex.ext.web.RoutingContext;
+import io.vertx.reactivex.ext.web.api.contract.openapi3.OpenAPI3RouterFactory;
+import io.vertx.reactivex.ext.web.handler.CorsHandler;
+import io.vertx.reactivex.ext.web.handler.sockjs.SockJSHandler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.apache.http.HttpHeaders.*;
+import static org.apache.http.entity.ContentType.*;
+import static io.netty.handler.codec.http.HttpResponseStatus.*;
 
 public class MainVerticle extends AbstractVerticle {
 
-  HttpServer server;
+  private static final Logger LOG = LoggerFactory.getLogger(MainVerticle.class);
+  private static final String PHOTO_SVC_ADDRESS = "service.photos";
+  private static final String USER_SVC_ADDRESS = "service.users";
+  private static final String GROUP_SVC_ADDRESS = "service.groups";
+  public static final String OPENAPI_SPEC = "/openapi.yaml";
 
-  @Override
-  public void start(Future future) {
-    OpenAPI3RouterFactory.create(this.vertx, "/openapi.yml", openAPI3RouterFactoryAsyncResult -> {
-      if (openAPI3RouterFactoryAsyncResult.succeeded()) {
-        OpenAPI3RouterFactory routerFactory = openAPI3RouterFactoryAsyncResult.result();
+  JsonObject loadedConfig;
 
-        // Enable automatic response when ValidationException is thrown
-        // routerFactory.setValidationFailureHandler(true);
+  Maybe<JsonObject> initConfigRetriever() {
+    // Load the default configuration from the classpath
+    LOG.info("Configuration store loading.");
+    ConfigStoreOptions defaultOpts = new ConfigStoreOptions().setType("file").setFormat("json")
+            .setConfig(new JsonObject().put("path", "insult_default_config.json"));
 
-        // Add routes handlers
-        routerFactory.addHandlerByOperationId("userLogin", new com.zanclus.photo.handlers.UserLoginHandler());
-        routerFactory.addHandlerByOperationId("newUser", new com.zanclus.photo.handlers.NewUserHandler());
-        routerFactory.addHandlerByOperationId("getUserDetailsByName", new com.zanclus.photo.handlers.GetUserDetailsByNameHandler());
-        routerFactory.addHandlerByOperationId("newGroup", new com.zanclus.photo.handlers.NewGroupHandler());
-        routerFactory.addHandlerByOperationId("getGroupById", new com.zanclus.photo.handlers.GetGroupByIdHandler());
-        routerFactory.addHandlerByOperationId("updateGroupById", new com.zanclus.photo.handlers.UpdateGroupByIdHandler());
-        routerFactory.addHandlerByOperationId("addUserToGroup", new com.zanclus.photo.handlers.AddUserToGroupHandler());
-        routerFactory.addHandlerByOperationId("deleteUserFromGroup", new com.zanclus.photo.handlers.DeleteUserFromGroupHandler());
-        routerFactory.addHandlerByOperationId("addPhoto", new com.zanclus.photo.handlers.AddPhotoHandler());
-        routerFactory.addHandlerByOperationId("getImageById", new com.zanclus.photo.handlers.GetImageByIdHandler());
-        routerFactory.addHandlerByOperationId("updateImageById", new com.zanclus.photo.handlers.UpdateImageByIdHandler());
-        routerFactory.addHandlerByOperationId("deleteImageById", new com.zanclus.photo.handlers.DeleteImageByIdHandler());
-        routerFactory.addHandlerByOperationId("getTagsByPhotoId", new com.zanclus.photo.handlers.GetTagsByPhotoIdHandler());
-        routerFactory.addHandlerByOperationId("addTagToPhoto", new com.zanclus.photo.handlers.AddTagToPhotoHandler());
-        routerFactory.addHandlerByOperationId("deleteTagFromPhoto", new com.zanclus.photo.handlers.DeleteTagFromPhotoHandler());
+    // When running inside of Kubernetes, configure the application to also load
+    // from a ConfigMap. This config is ONLY loaded when running inside of
+    // Kubernetes or OpenShift
+    ConfigStoreOptions confOpts = new ConfigStoreOptions().setType("configmap")
+            .setConfig(new JsonObject().put("name", "insult-config").put("optional", true));
 
-        // Add security handlers
-        routerFactory.addSecurityHandler("BasicAuth", new com.zanclus.photo.securityHandlers.BasicAuthSecurityHandler());
-        routerFactory.addSecurityHandler("BearerAuth", new com.zanclus.photo.securityHandlers.BearerAuthSecurityHandler());
-        routerFactory.addSecurityHandler("ApiKeyAuth", new com.zanclus.photo.securityHandlers.ApiKeyAuthSecurityHandler());
+    // Add the default and container config options into the ConfigRetriever
+    ConfigRetrieverOptions retrieverOptions = new ConfigRetrieverOptions()
+            .addStore(defaultOpts)
+            .addStore(confOpts);
 
-        // Generate the router
-        Router router = routerFactory.getRouter();
-        server = vertx.createHttpServer(new HttpServerOptions().setPort(8080).setHost("localhost"));
-        server.requestHandler(router::accept).listen();
-        future.complete();
-      } else {
-          // Something went wrong during router factory initialization
-          Throwable exception = openAPI3RouterFactoryAsyncResult.cause();
-      }
-    });
+    // Create the ConfigRetriever and return the Maybe when complete
+    return ConfigRetriever.create(vertx, retrieverOptions).rxGetConfig().toMaybe();
+  }
+
+  /**
+   * Begin the creation of the {@link OpenAPI3RouterFactory}
+   *
+   * @param config The config loaded via the {@link ConfigRetriever}
+   * @return An {@link OpenAPI3RouterFactory} {@link Future} to be used to
+   *         complete the next Async step
+   */
+  private Maybe<OpenAPI3RouterFactory> provisionRouter(JsonObject config) {
+    // Merge the loaded configuration into the config for this Verticle
+    loadedConfig = config().mergeIn(config);
+
+    if (LOG.isInfoEnabled()) {
+      LOG.info("Config Loaded: {}", loadedConfig.encodePrettily());
+    }
+
+    // Instantiate the services and bind them to the event bus
+
+    // Create the OpenAPI3RouterFactory using the API specification YAML file
+    return OpenAPI3RouterFactory.rxCreate(vertx, OPENAPI_SPEC).toMaybe();
+  }
+
+  /**
+   * Wire the {@link OpenAPI3RouterFactory} into the HTTP Server
+   *
+   * @param factory The {@link OpenAPI3RouterFactory} created in the previous step
+   * @return An {@link HttpServer} if successful
+   */
+  Maybe<HttpServer> provisionHttpServer(OpenAPI3RouterFactory factory) {
+    // Configure the HTTP Server options
+    // - Listen on port 8080 on all interfaces using HTTP2 protocol
+    HttpServerOptions httpOpts = new HttpServerOptions()
+            .setHost(loadedConfig.getJsonObject("http").getString("address"))
+            .setPort(loadedConfig.getJsonObject("http").getInteger("port")).setReuseAddress(true)
+            .setSoLinger(0).setLogActivity(true);
+
+    // TODO: Get the service proxy instances
+
+    // Map out OpenAPI3 route to our Service Proxy implementation
+//    factory.addHandlerByOperationId("getInsult", ctx -> service.rxGetREST()
+//            .doOnError(e -> errorHandler(ctx, e)).subscribe(json -> sendResult(ctx, json)));
+
+    // Map out OpenAPI3 route to our Service Proxy implementation
+//    factory.addHandlerByOperationId("health", ctx -> service.rxCheck()
+//            .subscribe(
+//                    json -> sendResult(ctx, json),
+//                    e -> errorHandler(ctx, e)
+//            )
+//    );
+
+    Router api = factory.getRouter();
+
+    Router root = Router.router(vertx);
+
+    CorsHandler corsHandler = CorsHandler.create(".*").allowedHeader("Access-Control-Request-Method")
+            .allowedHeader("Access-Control-Allow-Origin")
+            .allowedHeader("Access-Control-Allow-Headers").allowedHeader("Content-Type")
+            .allowedMethod(HttpMethod.GET).allowedMethod(HttpMethod.POST)
+            .allowedMethod(HttpMethod.HEAD).allowedMethod(HttpMethod.PUT)
+            .allowedMethod(HttpMethod.OPTIONS).allowedMethod(HttpMethod.CONNECT);
+
+    root.route().order(0).handler(corsHandler);
+    api.route().order(0).handler(corsHandler);
+
+    root.mountSubRouter("/api/v1", api);
+
+    BridgeOptions bOpts = new BridgeOptions()
+            .addInboundPermitted(new PermittedOptions().setAddress(USER_SVC_ADDRESS))
+            .addInboundPermitted(new PermittedOptions().setAddress(PHOTO_SVC_ADDRESS))
+            .addInboundPermitted(new PermittedOptions().setAddress(GROUP_SVC_ADDRESS))
+            .addOutboundPermitted(new PermittedOptions().setAddress(USER_SVC_ADDRESS))
+            .addOutboundPermitted(new PermittedOptions().setAddress(PHOTO_SVC_ADDRESS))
+            .addOutboundPermitted(new PermittedOptions().setAddress(GROUP_SVC_ADDRESS));
+
+    SockJSHandler sockHandler = SockJSHandler.create(vertx).bridge(bOpts);
+
+    root.route("/eventbus/*").handler(sockHandler);
+
+    return vertx.createHttpServer(httpOpts).requestHandler(root::handle).rxListen().toMaybe();
+  }
+
+  /**
+   * Send a successful HTTP response
+   *
+   * @param ctx  The {@link RoutingContext} of the request
+   * @param json The {@link JsonObject} body to be sent in the response
+   */
+  private void sendResult(RoutingContext ctx, JsonObject json) {
+    ctx.response().putHeader(CONTENT_TYPE, APPLICATION_JSON.getMimeType())
+            .setStatusCode(OK.code()).setStatusMessage(OK.reasonPhrase())
+            .end(json.encodePrettily());
+  }
+
+  /**
+   * Send an unsuccessful HTTP response
+   *
+   * @param ctx The {@link RoutingContext} of the request
+   * @param e   The error which caused the failure
+   */
+  private void errorHandler(RoutingContext ctx, Throwable e) {
+    ctx.response().setStatusCode(INTERNAL_SERVER_ERROR.code())
+            .setStatusMessage(INTERNAL_SERVER_ERROR.reasonPhrase()).end(e.getLocalizedMessage());
   }
 
   @Override
-  public void stop(){
-    this.server.close();
+  public void start(Future startFuture) {
+    initConfigRetriever()
+            .flatMap(this::provisionRouter)
+            .flatMap(this::provisionHttpServer)
+            .subscribe(
+                    c -> startFuture.complete(),
+                    startFuture::fail
+            );
   }
-
 }
