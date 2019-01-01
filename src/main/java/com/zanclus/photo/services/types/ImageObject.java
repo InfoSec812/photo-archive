bb0e47ca1581b075cccda5d41772cf601774034e
Deven Phillips <deven.phillips@redhat.com>
2019-01-01 14:14:57 -0500

    Add Lombok Delegate for ImageObject

--- src/main/java/com/zanclus/photo/services/types/ImageObject.java
+++ src/main/java/com/zanclus/photo/services/types/ImageObject.java
@@ -4,13 +4,16 @@ import io.vertx.codegen.annotations.DataObject;
 import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 import lombok.Data;
+import lombok.experimental.Delegate;
 
 @Data
 @DataObject
 public class ImageObject extends VertxDataObject {
 
+    @Delegate(types = com.google.schemaorg.core.ImageObject.class)
+    com.google.schemaorg.core.ImageObject instance;
+
     public ImageObject(JsonObject json) {
         ImageObject temp = Json.decodeValue(json.encode(), ImageObject.class);
     }
-
 }
